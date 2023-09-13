d7c1392aff5122b19ee9786e7183858f5f33a04e
Pey2516 <prhall1@umary.edu>
2023-09-12 22:50:43 -0500

    Added Lab01A_FirstApp.java

--- Lab01A_FirstApp.java
+++ Lab01A_FirstApp.java
@@ -3,6 +3,7 @@ public class Lab01A_FirstApp
     public static void main ( String[] args )
     {
         System.out.println("CSC356: Programming Languages I\n Professor Volkman\n Peyton Hall");
-        //Prints out what is in parentheses. The \n sends the whatever is after it to the next line.
+        //Prints out what is in parentheses. The \n sends the whatever is after it to the next line.clear
+        
     }
 }
