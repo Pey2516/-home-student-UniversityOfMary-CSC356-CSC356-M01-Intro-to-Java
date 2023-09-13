d4224384361cf44d8195162e9b982a95786e99a2
Pey2516 <prhall1@umary.edu>
2023-09-12 21:25:14 -0500

    Added Lab01A_Temperature.java file

--- Lab01A_Temperature.java
+++ Lab01A_Temperature.java
@@ -4,19 +4,19 @@ public class Lab01A_Temperature
 {
     public static void main ( String[] args )
     {
-        try (Scanner keyboard = new Scanner ( System.in )) {
-            double C;
-            double F;
+        Scanner keyboard = new Scanner ( System.in );
+        double C;
+        double F;
 
-            // Output String, leave cursor at the end of the line
-            System.out.println( "Enter Desired Number In Celsius: " );
+        System.out.println( "Enter Desired Number In Celsius: " );
 
-            // Read Integer value from the keyboard and store the number entered to ‘input’
-            C = keyboard.nextDouble();
+        // Read Integer value from the keyboard and store the number entered
+        C = keyboard.nextDouble();
 
-            F = (9.0/5.0) * C +32;
+        //Equation for convertion of Celsius to Fahrenheit
+        F = (9.0/5.0) * C +32;
 
-            System.out.println ("Your Celsius Degree in Fahrenheit : " + F);
-        }
+        //Tells you the integer you inputed into Fahrenheit
+        System.out.println ("Your Celsius Degree in Fahrenheit : " + F);
     }
 }
