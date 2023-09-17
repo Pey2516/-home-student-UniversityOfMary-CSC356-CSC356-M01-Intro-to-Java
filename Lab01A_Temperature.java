95d59341731fb47b21521f737919125f86f0b47b
Pey2516 <prhall1@umary.edu>
2023-09-17 14:14:18 -0500

    Added Lab01A_Temperature.java file

--- Lab01A_Temperature.java
+++ Lab01A_Temperature.java
@@ -8,15 +8,16 @@ public class Lab01A_Temperature
         double C;
         double F;
 
+        //Asks what number you want in Celsius.
         System.out.println( "Enter Desired Number In Celsius: " );
 
-        // Read Integer value from the keyboard and store the number entered
+        // Read Integer value from the keyboard and store the number entered.
         C = keyboard.nextDouble();
 
-        //Equation for convertion of Celsius to Fahrenheit
+        //Equation for convertion of Celsius to Fahrenheit.
         F = (9.0/5.0) * C +32;
 
-        //Tells you the integer you inputed into Fahrenheit
+        //Tells you the integer you inputed into Fahrenheit.
         System.out.println ("Your Celsius Degree in Fahrenheit : " + F);
     }
 }
